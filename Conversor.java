4a2a912882d4bac0e788e4288de2bcf4291502dd
Lucas Oliveira <86750585+OliverLuucas@users.noreply.github.com>
2022-01-20 21:14:45 -0300

    Add files via upload

---
+++ Conversor.java
@@ -0,0 +1,10 @@
+
+public class Conversor{
+
+    public static void main(String[] args){
+    int num1 = 50;
+    System.out.println(num1);
+    short num = new Short((short) num1);
+    System.out.println(num);
+    }
+}
